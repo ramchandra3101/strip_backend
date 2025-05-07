a44542bf80dfbe595e9c6850433926147b7c18dc
ramchandra3101 <ramchandra3101@gmail.com>
2025-05-07 13:27:18 -0400

    remoiving .env

--- app/core/config.py
+++ app/core/config.py
@@ -22,7 +22,7 @@ class Settings(BaseSettings):
     AWS_ACCESS_KEY_ID : str
     AWS_SECRET_ACCESS_KEY : str
     AWS_REGION : str
-    S3_BUCKET : str = "striplens"
+    S3_BUCKET : str = "striplensapp"
 
     DEV_MODE: bool = True
 
